b2b509f75e61b9b50ad84b5d6405dc86671e007a
benjitax777 <benjaminsaezmarambio@gmail.com>
2026-05-05 00:03:12 -0400

    Update ejercicios.py

--- conceptos_basicos/parcial/ejercicios.py
+++ conceptos_basicos/parcial/ejercicios.py
@@ -65,7 +65,39 @@ else:
             print(f"Monto final: ${monto_final}")
         else:
             print(f"no hay descuento tienes que pagar: {monto}")
-            
+                     
+nombre = input("Ingrese su nombre ")
+patente = input("Ingrese su patente ")
+
+if len(patente) != 6:
+    print("Ingrese una patente correcta por favor")
+else:
+    vehiculo = input("Ingrese su tipo de vehiculo ").lower()
+    horas = int(input("Ingrese las horas en la que estuvo estacionado "))
+
+    if vehiculo == "auto":
+        costo = 1500 * horas
+        if horas > 8:
+            costo = costo - (costo * 0.20)
+        print(f"{nombre}, la patente {patente} debe pagar: ${costo}")
+
+    elif vehiculo == "moto":
+        costo = 800 * horas
+        if horas > 8:
+            costo = costo - (costo * 0.20)
+        print(f"{nombre}, la patente {patente} debe pagar: ${costo}")
+
+    elif vehiculo == "camion":
+        costo = 3000 * horas
+        if horas > 8:
+            costo = costo - (costo * 0.20)
+        print(f"{nombre}, la patente {patente} debe pagar: ${costo}")
+
+    else:
+        print("Tipo de vehiculo no reconocido")
+
+    
+
 
 
 
