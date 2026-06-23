409342c07f09eac4c9240716fb239c3380ef34fb
benjitax777 <benjaminsaezmarambio@gmail.com>
2026-06-23 03:27:43 -0400

    Practique sobre funciones

---
+++ Condicionales.py
@@ -0,0 +1,66 @@
+
+#Condicionales
+"""
+edad=int(input("Ingrese su edad: "))
+
+if edad >=65:
+    print("usted es mayor de edad")
+elif edad <=13:
+    print("usted es niño")
+elif edad <= 18:
+    print("usted adolecente")
+elif edad <= 64:
+    print("usted es adulto")
+"""
+
+
+
+
+
+
+
+
+
+
+"""
+try:
+    a=int(input("Ingrese  numero 1 : "))
+except ZeroDivisionError:
+    print("No se puede divir en cero")
+except ValueError:
+    print("ingrese un numero por favor")
+try:
+    b=int(input("Ingrese  numero 2 : "))
+except ZeroDivisionError:
+     print("No se puede divir en cero")
+except ValueError:
+    print("ingrese un numero por favor")
+Operacion=input("ingrese una operacion (+, -, *, /): ")
+
+
+if Operacion =="+":
+    resultado=a+b
+    print(f"El Resultado fue: {resultado}")
+elif Operacion == "-":
+    resultado=a-b
+    print(f"El Resultado fue: {resultado}")
+elif Operacion =="*":
+    resultado=a*b
+    print(f"El Resultado fue: {resultado}")
+elif  Operacion == "/":
+   
+   resultado=a/b
+   print(f"El Resultado fue: {resultado}")
+else:
+    print("Error elija una operacion correcta")
+"""
+
+
+
+
+
+
+
+
+    
+
---
+++ Parcial 4/Untitled-1.py
@@ -0,0 +1,76 @@
+
+"""
+inventario={
+    "P001": ["Television", "20000",10],
+    "P002": ["PC Gamer", "1000000",3],
+    "P003": ["PLAY 5", "500000",4]
+}
+def actualizar_stock(codigo, cantidad_nueva):
+    
+
+
+    if  codigo in inventario:#El in permite verifical la existencia de la llave sin necesidad de for
+        inventario[codigo][2]= inventario[codigo][2] + cantidad_nueva
+        return True
+    else:
+        print("Error, no se encontro el codigo")
+        return False
+            
+resultado=actualizar_stock("P001",3)
+print(resultado)
+"""
+
+
+
+
+"""
+
+inventario={
+    "RUT 2248548-7": [4.5,4.5,7.0],
+    "RUT 5541541-5": [7.0,5.5,4.4]
+}
+
+def calcular_promedios():
+    promedios={}
+    for rut, notas in inventario.items():
+        
+        sum(notas)
+        len(notas)
+        promedio=round(sum(notas)/len(notas),1)
+        promedios[rut]=promedio
+    return promedios
+
+resultado=calcular_promedios()
+print(resultado)
+"""
+
+
+
+
+
+
+biblioteca = {
+    "978-01": ["El Hobbit", "J.R.R. Tolkien", "Fantasía"],
+    "978-02": ["It", "Stephen King", "Terror"],
+    "978-03": ["Dune", "Frank Herbert", "Ciencia Ficción"],
+    "978-04": ["El Resplandor", "Stephen King", "Terror"],
+    "978-05": ["Fundación", "Isaac Asimov", "Ciencia Ficción"]
+}
+
+
+def buscar_por_genero(genero_buscado):
+    
+    encontrado=False
+    for titulos in biblioteca:
+        if biblioteca[titulos][2].lower() == genero_buscado.lower():
+            caca=biblioteca[titulos][0]
+            autor=biblioteca[titulos][1]
+            print(f"Título: {caca}, Autor: {autor}")
+            encontrado=True
+    if not encontrado:
+
+        print("No hay libros disponibles en este genero")
+            
+   
+
+buscar_por_genero("TERROR")
